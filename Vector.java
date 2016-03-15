f5e6d4d5e1d864a333942e7a0966e10ec96e7aa5
MaGa <guser1996.gu@gmail.com>
2016-03-15 14:47:30 +0100

    added to vector: checker for direction (equal, opposite), check for equality and getter for opposite vector

--- Vector.java
+++ Vector.java
@@ -2,8 +2,10 @@
 import java.util.*;
 
 /**
- * class storing a set of coordinates
+ * class storing a set of double coordinates
  * in order to identify a position
+ * values are rounded to the closest digit according to 
+ * static field DIGITS_ACCURACY
  * @author martin
  */
 public class Vector
@@ -30,6 +32,30 @@ public class Vector
 		
 		public BadDimensionException (String message) { super (message); }
 	}
+	
+	/**
+	 * @param num number to round
+	 * @return num rounded to closest DIGITS_ACCURACY th digit
+	 */
+	public static double round (double num)
+	{
+		double power = Math.pow (10, DIGITS_ACCURACY);
+		return (Math.round (power * num) / power);
+	}
+	
+	/**
+	 * @param n1 first number
+	 * @param n2 second number
+	 * @return true if n1 and n2 are considered equal given a margin of error
+	 * smaller than the accuracy used
+	 */
+	public static boolean compareEpsilon (double n1, double n2)
+	{
+		double eps = 1.0 / Math.pow (10, DIGITS_ACCURACY);
+		return (n1 >= n2 - eps && n1 <= n2 + eps);
+	}
+	
+	public static int DIGITS_ACCURACY = 3;
 	
 	/**
 	 * @param dim dimension point is in
@@ -64,6 +90,17 @@ public class Vector
 	}
 	
 	/**
+	 * @return vector in opposite direction
+	 */
+	public Vector getOppositeVector()
+	{
+		ArrayList<Double> negValues = new ArrayList<>();
+		for (int cDim = 0; cDim < getDimension(); ++cDim)
+			negValues.set (cDim, -getCoordinate (cDim));
+		return new Vector (negValues);
+	}
+	
+	/**
 	 * @return internal array list used to store position
 	 */
 	public ArrayList<Double> getCoordinates() { return mPos; }
@@ -79,6 +116,52 @@ public class Vector
 	 * @return dimension of point (number of coordinates)
 	 */
 	public int getDimension() { return mPos.size(); }
+	
+	/**
+	 * @param v vector to compare to this vector
+	 * @return true if v and this have the same orientation
+	 * i.e. if they have the same ratio over all coordinates
+	 * and this ratio is always positive
+	 */
+	public boolean isSameOrientation (Vector v)
+	{
+		double scalar = this.getCoordinate (0) / v.getCoordinate (0);
+		
+		for (int cDim = 1; cDim < getDimension() - 1; ++cDim)
+		{
+			//negative ratio
+			if (scalar < 0)
+				return false;
+			double newScalar = this.getCoordinate (cDim) / v.getCoordinate (cDim);
+			//different ratio
+			if (!compareEpsilon (scalar, newScalar))
+				return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * @param v vector to compare to this vector
+	 * @return true if v and this have the opposite orientation
+	 * i.e. if they have the opposite ratio over all coordinates
+	 * and this ratio is always negative
+	 */
+	public boolean isOppositeOrientation (Vector v)
+	{
+		double scalar = -this.getCoordinate (0) / v.getCoordinate (0);
+		
+		for (int cDim = 1; cDim < getDimension() - 1; ++cDim)
+		{
+			//negative ratio
+			if (scalar < 0)
+				return false;
+			double newScalar = -this.getCoordinate (cDim) / v.getCoordinate (cDim);
+			//different ratio
+			if (!compareEpsilon (scalar, newScalar))
+				return false;
+		}
+		return true;
+	}
 	
 	/**
 	 * @param add position to add to current one
@@ -99,7 +182,7 @@ public class Vector
 	{
 		if (index < 0 || index >= getDimension())
 			throw new IllegalIndexException ("inex " + index + " is " + (index < 0 ? " less than zero " : " too large"));
-		mPos.set (index, mPos.get (index) + value);
+		mPos.set (index, round (mPos.get (index) + value));
 	}
 	
 	
